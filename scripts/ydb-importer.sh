30f3bb5bd9d715a6d71af6450331bef1a6372ae0
Maksim Zinal <maxzinal@yandex.ru>
2022-10-17 12:57:54 +0300

    excluded JDBC driver dependencies on build

--- scripts/ydb-importer.sh
+++ scripts/ydb-importer.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
+# For YDB in Yandex Cloud:
+#   https://cloud.yandex.com/en/docs/iam/operations/authorized-key/create
 # export YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS=$(pwd)/yc-fednew-iot1sa1.json
 
 # -Djavax.net.ssl.trustStore=work-cas.dat -Djavax.net.ssl.trustStorePassword=passw0rd
-java -Xms256m -Xmx2048m -jar lib/ydb-importer-* $@
+java -Xms256m -Xmx8192m -classpath 'lib/*' tech.ydb.importer.YdbImporter $@
